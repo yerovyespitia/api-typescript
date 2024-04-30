a781eca3949a5fd83210c48f7ab8297e9cdb8882
Yerovy Espitia <luyeesgar@gmail.com>
2024-04-30 17:28:44 -0500

    add mongodb & user schema & user controllers

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
 
 const app = express()
 
@@ -17,6 +19,15 @@ app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
 
+dotenv.config()
+
 const server = http.createServer(app)
 
 server.listen(4000, () => console.log('Server running on port 4000'))
+
+const MONGO_URL = process.env.MONGO_URL
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL).then(() => console.log('Connected to mongodb'))
+
+mongoose.connection.on('error', (error: Error) => console.log(error))
